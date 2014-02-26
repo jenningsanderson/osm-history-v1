7836d1b62fe935c0e2ac7a688712e74f9751c658
Jennings Anderson <jennings.anderson@colorado.edu>
2014-02-25 20:27:53 -0700

    Now it initializes to MongoDB

--- ruby/read_pbf.rb
+++ ruby/read_pbf.rb
@@ -1,21 +1,52 @@
+'''
+PBF Parser from: https://github.com/planas/pbf_parser
+'''
+
 require 'pbf_parser'
+require 'mongo'
 
-parser = PbfParser.new("/Users/jenningsanderson/Downloads/nepal.osm.pbf")
-
-n_count = 0
-w_count = 0
-r_count = 0
-
-while parser.next
-	unless parser.nodes.empty?
-		n_count+= parser.nodes.size
-	end
-	unless parser.ways.empty?
-		w_count+= parser.ways.size
-	end
-	unless parser.relations.empty?
-		r_count+= parser.relations.size
-	end
+def connect_to_mongo
+	client = Mongo::MongoClient.new
+	db = client['example-db'] 
+	coll = db['example-collection']
+	10.times { |i| coll.insert({ :count => i+1 }) }
+	puts "There are #{coll.count} total documents. Here they are:"
+	coll.find.each { |doc| puts doc.inspect }
 end
 
+connect_to_mongo
+
+
+def parse_test
+	parser = PbfParser.new("/Users/jenningsanderson/Downloads/nepal.osm.pbf")
+
+	n_count = 0
+	w_count = 0
+	r_count = 0
+
+	while parser.next
+		unless parser.nodes.empty?
+			n_count+= parser.nodes.size
+		end
+		unless parser.ways.empty?
+			w_count+= parser.ways.size
+		end
+		unless parser.relations.empty?
+			r_count+= parser.relations.size
+		end
+	end
 puts "Nodes: #{n_count}, Ways: #{w_count}, Rels: #{r_count}"
+
+end
+
+'''
+TODO: Intelligently convert nodes to points, ways to lines, and relations
+to polygons
+
+Create these fields and then push the file to MongoDB.  From there we 
+can make spatial queries
+'''
+
+'''
+Connect to MongoDB
+'''
