d89a34ccfdc63a369fdc93d9205d3b26d24b9c57
Jennings Anderson <jennings.anderson@colorado.edu>
2014-04-25 01:22:44 -0600

    updated the r code for the changesets

--- rcode/haiti_changesets.r
+++ rcode/haiti_changesets.r
@@ -1,67 +1,66 @@
 # Attempting to visualize this data with R
 
-# install package to connect through monodb
-# install.packages("rmongodb")
-library(rmongodb) #Loads the mongodb library
+library(rmongodb) # Loads the mongodb library
+library(plyr)     # Need this for a dataframe
 
 # Connect to MongoDB
 mongo = mongo.create(host = "epic-analytics.cs.colorado.edu:27018")
 mongo.is.connected(mongo)
 
-# Show the databases available (each is a specific region)
-# mongo.get.databases(mongo)
 
-# Show the collections available
-# mongo.get.database.collections(mongo, db = "haiti")
-
-# Query for the data we want:
-query = mongo.bson.buffer.create()
-startDate <- ISOdate(2010,01,12)
-endDate <- ISOdate(2010,02,12)
-mongo.bson.buffer.start.object(query, "created_at")
-mongo.bson.buffer.append(query, "$gt", startDate)
-mongo.bson.buffer.append(query, "$lt", endDate)
-mongo.bson.buffer.finish.object(query)
-query = mongo.bson.from.buffer(query)
-
-# define the fields we want
-fields = mongo.bson.buffer.create()
-mongo.bson.buffer.append(fields, "uid", 1L)
-mongo.bson.buffer.append(fields, "node_count", 1L)
-mongo.bson.buffer.append(fields, "node_density", 1L)
-mongo.bson.buffer.append(fields, "created_at", 1L)
-mongo.bson.buffer.append(fields, "area", 1L)
-mongo.bson.buffer.append(fields, "_id", 0L)
-fields = mongo.bson.from.buffer(fields)
-
-## create the namespace
-DBNS= "haiti.changesets"
-
-#Count the number of records in the namespace
-size <- mongo.count(mongo, ns = DBNS, query)
-
-pb <- txtProgressBar(min = 0, max = size, style = 3)
-
-#Create the cursor
-cursor = mongo.find(mongo, ns=DBNS, query=query, fields=fields)
-
-## iterate over the cursor
-changesetInfo = data.frame(stringsAsFactors = FALSE)
-i <- 0
-while(mongo.cursor.next(cursor)){
-  #Read the value
-  tmp = mongo.bson.to.list(mongo.cursor.value(cursor))
-  # Add it to the dataframe
-  changesetInfo = rbind.fill(changesetInfo, as.data.frame(tmp))
-  #Show the status message
-  i = i+1
-  setTxtProgressBar(pb, i)
+# Get our cursor
+timeboxed_cursor <- function(startDate, endDate, DBNS, q_limit=500000){
+  
+  # First, define the query
+  query = mongo.bson.buffer.create()
+  #startDate <- ISOdate(2010,01,12)
+  #endDate <- ISOdate(2010,02,12)
+  mongo.bson.buffer.start.object(query, "created_at")
+  mongo.bson.buffer.append(query, "$gt", startDate)
+  mongo.bson.buffer.append(query, "$lt", endDate)
+  mongo.bson.buffer.finish.object(query)
+  query = mongo.bson.from.buffer(query)
+  
+  # Next, define the fields we want for our dataframe
+  fields = mongo.bson.buffer.create()
+  mongo.bson.buffer.append(fields, "uid", 1L)
+  mongo.bson.buffer.append(fields, "node_count", 1L)
+  mongo.bson.buffer.append(fields, "node_density", 1L)
+  mongo.bson.buffer.append(fields, "created_at", 1L)
+  mongo.bson.buffer.append(fields, "area", 1L)
+  mongo.bson.buffer.append(fields, "_id", 0L)
+  fields = mongo.bson.from.buffer(fields)
+  
+  cursor = mongo.find(mongo, ns=DBNS, query=query, fields=fields, limit=q_limit)
+  size <<- mongo.count(mongo, ns=DBNS, query)
+  return(cursor)
 }
-close(pb)
 
-hist(log(changesetInfo$node_count, base=10), col='blue', main="Frequency of Changesets with #Nodes", xlab="Log_10 Node Count", ylab="Freq")
-hist(log(changesetInfo$node_density, base=10), col='red', main="Frequency of Nodes/Area", xlab="Log_10 Changeset Count", ylab="Freq")
-hist(log(changesetInfo$area, base=10), col='purple', main="Frequency of Area Size (Sq. km)", xlab="Log_10 Changeset Count", ylab="Freq")
-hist(changesetInfo$created_at, breaks='day', col='orange', main="Times of Edits", xlab="When", ylab="Changesets")
+buildChangeset <- function(cursor){
+  changesetInfo = data.frame(stringsAsFactors = FALSE)
+  pb <- txtProgressBar(min = 0, max = size, style = 3)
+  i <- 0
+  while(mongo.cursor.next(cursor)){
+    #Read the value
+    tmp = mongo.bson.to.list(mongo.cursor.value(cursor))
+    # Add it to the dataframe
+    changesetInfo = rbind.fill(changesetInfo, as.data.frame(tmp))
+    #Show the status message
+    i = i+1
+    setTxtProgressBar(pb, i)
+  }
+  close(pb)
+  return(changesetInfo)
+}
+
+# Testing my functions
+
+#cursor = timeboxed_cursor(ISOdate(2012,01,12), ISOdate(2012,02,12), 'haiti.changesets', q_limit=500)
+#info = buildChangeset(cursor)
+
+#hist(log(info$node_count, base=10), col='blue', main="Haiti: Frequency of Changesets with #Nodes", xlab="Log_10 Node Count", ylab="Freq")
+#hist(log(haitiChangesetInfo$node_density, base=10), col='red', main="Haiti: Frequency of Nodes/Area", xlab="Log_10 Changeset Count", ylab="Freq")
+#hist(log(haitiChangesetInfo$area, base=10), col='purple', main="Haiti: Frequency of Area Size (Sq. km)", xlab="Log_10 Changeset Count", ylab="Freq")
+#hist(haitiChangesetInfo$created_at, breaks='day', col='orange', main="Haiti: Times of Edits", xlab="When", ylab="Changesets")
 
 
--- rcode/phil_changesets.R
+++ rcode/phil_changesets.R
@@ -35,33 +35,33 @@ mongo.bson.buffer.append(fields, "_id", 0L)
 fields = mongo.bson.from.buffer(fields)
 
 ## create the namespace
-DBNS= "philippines.changesets"
+philDBNS= "philippines.changesets"
 
 #Count the number of records in the namespace
-size <- mongo.count(mongo, ns = DBNS, query)
+size <- mongo.count(mongo, ns = philDBNS, query)
 
 pb <- txtProgressBar(min = 0, max = size, style = 3)
 
 #Create the cursor
-cursor = mongo.find(mongo, ns=DBNS, query=query, fields=fields)
+cursor = mongo.find(mongo, ns=philDBNS, query=query, fields=fields)
 
 ## iterate over the cursor
-changesetInfo = data.frame(stringsAsFactors = FALSE)
+philChangesetInfo = data.frame(stringsAsFactors = FALSE)
 i <- 0
 while(mongo.cursor.next(cursor)){
   #Read the value
   tmp = mongo.bson.to.list(mongo.cursor.value(cursor))
   # Add it to the dataframe
-  changesetInfo = rbind.fill(changesetInfo, as.data.frame(tmp))
+  philChangesetInfo = rbind.fill(philChangesetInfo, as.data.frame(tmp))
   #Show the status message
   i = i+1
   setTxtProgressBar(pb, i)
 }
 close(pb)
 
-hist(log(changesetInfo$node_count, base=10), col='blue', main="Frequency of Changesets with #Nodes", xlab="Log_10 Node Count", ylab="Freq")
-hist(log(changesetInfo$node_density, base=10), col='red', main="Frequency of Nodes/Area", xlab="Log_10 Changeset Count", ylab="Freq")
-hist(log(changesetInfo$area, base=10), col='purple', main="Frequency of Area Size (Sq. km)", xlab="Log_10 Changeset Count", ylab="Freq")
-hist(changesetInfo$created_at, breaks='day', col='orange', main="Times of Edits", xlab="When", ylab="Changesets")
+hist(log(philChangesetInfo$node_count, base=10), col='blue', main="Phil: Frequency of Changesets with #Nodes", xlab="Log_10 Node Count", ylab="Freq")
+hist(log(philChangesetInfo$node_density, base=10), col='red', main="Phil: Frequency of Nodes/Area", xlab="Log_10 Changeset Count", ylab="Freq")
+hist(log(philChangesetInfo$area, base=10), col='purple', main="Phil: Frequency of Area Size (Sq. km)", xlab="Log_10 Changeset Count", ylab="Freq")
+hist(philChangesetInfo$created_at, breaks='day', col='orange', main="Phil: Times of Edits", xlab="When", ylab="Changesets")
 
 
